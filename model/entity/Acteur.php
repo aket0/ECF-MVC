b7faeace8e12695ae52fa58986b4fac004e70bd8
asmaYh <64766382+asmaYh@users.noreply.github.com>
2024-05-13 14:06:05 +0200

    ajout Acteur

---
+++ model/entity/Acteur.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Model\entity;
+
+/**
+ * Description de Acteur
+ *
+ */
+class Offre
+{
+    private $id;
+    private $nom;
+    private $prenom;
+
+    public function __construct($id, $nom, $prenom)
+    {
+        $this->setId($id);
+        $this->setNom($nom);
+        $this->setPrenom($prenom);
+    }
+
+
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+    }
+
+    /**
+     * Get the value of nom
+     */ 
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set the value of nom
+     *
+     */ 
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+     
+    }
+
+    /**
+     * Get the value of prenom
+     */ 
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set the value of prenom
+     *
+     */ 
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+    }
+}
