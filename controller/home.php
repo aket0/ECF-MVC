a3cac0c0463429c36216cdf33e0e5b142b4e7eaa
asmaYh <64766382+asmaYh@users.noreply.github.com>
2024-05-14 13:56:35 +0200

    recherche

--- controller/home.php
+++ controller/home.php
@@ -11,4 +11,4 @@ unset($_SESSION['user']);
 // $_SESSION['user'] = 'vince@afpa.com';
 
 //On affiche le template Twig correspondant
-echo $twig->render('home.html.twig', ['offres' => $films]);
+echo $twig->render('home.html.twig', ['films' => $films]);
