65016ec33da93287213339ae3413e1d82de35e03
asmaYh <64766382+asmaYh@users.noreply.github.com>
2024-05-14 13:34:56 +0200

    Update FilmDAO.php

--- model/repository/FilmDAO.php
+++ model/repository/FilmDAO.php
@@ -45,7 +45,7 @@ class FilmDAO extends Dao
     //Deleter 1 film par son id
     public static function deleteOne(int $id): bool
     {
-        $query = self::$bdd->prepare('DELETE FROM Film WHERE film.id = :id_film');
+        $query = self::$bdd->prepare('DELETE FROM Film WHERE id_Film = :id_Film');
         $query->execute(array(':id_film' => $id));
         return $query->rowCount() == 1 ? true : false;
     }
@@ -60,30 +60,34 @@ class FilmDAO extends Dao
         return $query->rowCount() == 1 ? true : false;
     }
 
-    
-    //Rechercher un film par titre
-    public static function searchOne(string $titre) : array
-    {
-        $query = self::$bdd->prepare('SELECT * FROM Film WHERE titre LIKE :titre');
-        $query->execute(array(':titre' => '%'.$titre.'%'));
-        $films = array();
-        if($query->rowCount() != 0){
-            while ($data = $query->fetch()) {
-            $films[] = new Film($data['id'], $data['titre'], $data['realisateur'], $data['affiche'], $data['annee']);
-            }
-        }
-       
-        return ($films);
-
-    }
-
     //Ajouter un role
     public static function addOneRole($data): bool
     {
-
-        $requete = 'INSERT INTO Role (personnage,acteur) VALUES (:personnage, :acteur)';
-        $valeurs = ['personnage' => $data->getPersonnage(), 'realisateur' => $data->getRealisateur(), 'affiche' => $data->getAffiche(), 'annee' => $data->getAnnee()];
+        $requete = 'INSERT INTO Role (personnage) VALUES (:personnage) WHERE id_Film = :id_Film AND id_Acteur = :id_Acteur ';
+        $valeurs = ['personnage' => $data->getPersonnage()];
         $insert = self::$bdd->prepare($requete);
         return $insert->execute($valeurs);
+    }
+    // //Verification de Role
+    // public static function getRole($personnage): Role
+    // {
+    //     $query = self::$bdd->prepare('SELECT * FROM Role WHERE personnage = :personnage');
+    //     $query->execute(array(':personnage' => $personnage));
+    //     $data = $query->fetch();
+    //     return new Role($data['id_Acteur'], $data['id_Film'], $data['id'], $data['personnage'],);
+    // }
+    //Rechercher un film par titre
+    public static function searchOne(string $titre): array
+    {
+        $query = self::$bdd->prepare('SELECT * FROM Film WHERE titre LIKE :titre');
+        $query->execute(array(':titre' => '%' . $titre . '%'));
+        $films = array();
+        if ($query->rowCount() != 0) {
+            while ($data = $query->fetch()) {
+                $films[] = new Film($data['id'], $data['titre'], $data['realisateur'], $data['affiche'], $data['annee']);
+            }
+        }
+
+        return ($films);
     }
 }
