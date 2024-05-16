94156088bc63eb4a68d6a0413a5b9893b6c48d58
Hubert Moyne <43304133+aket0@users.noreply.github.com>
2024-05-16 09:05:38 +0200

    LAST PUSH OF HUBERT

--- controller/header.php
+++ controller/header.php
@@ -2,6 +2,7 @@
 
 $userLoggedIn = isset($_SESSION['user']);
 
+
 // Charge le template Twig en lui passant la variable userLoggedIn
 echo $twig->render('header.html.twig', ['userLoggedIn' => $userLoggedIn]);
 ?>
