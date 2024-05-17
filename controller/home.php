a685af753566b4645c7390fbdcb1460279b96e04
asmaYh <64766382+asmaYh@users.noreply.github.com>
2024-05-17 11:09:51 +0200

    supprimer controller recherche

--- controller/home.php
+++ controller/home.php
@@ -6,16 +6,26 @@ use Model\repository\ActeurDAO;
 
 //On appelle la fonction getAll()
 $filmDao = new FilmDAO();
-$acteurDao = new ActeurDAO();
-
-$films = $filmDao->getAll();
 
 
+$message =null;
+
+if (isset($_POST['motCle'])) {
+    $films = $filmDao::searchOne($_POST['motCle']);
+
+    empty($films) ? $message="Aucun film correspendant à \"".$_POST['motCle']. " \"" : $message="Résultat(s) trouvé(s) :";
+    
+    
+}
+else {
+    $films = $filmDao->getAll();
+}
 //On affiche le template Twig correspondant
 echo $twig->render(
     'home.html.twig',
     [
         'films' => $films,
+        'message' => $message
 
 
     ]
