c1beeaa36610ba717f550e01c05c1126b6e7de4e
asmaYh <64766382+asmaYh@users.noreply.github.com>
2024-05-17 13:32:14 +0200

    ajout recherche

--- controller/home.php
+++ controller/home.php
@@ -11,7 +11,7 @@ $filmDao = new FilmDAO();
 $message =null;
 
 if (isset($_POST['motCle'])) {
-    $films = $filmDao::searchOne($_POST['motCle']);
+    $films = $filmDao::getAll($_POST['motCle']);
 
     empty($films) ? $message="Aucun film correspendant à \"".$_POST['motCle']. " \"" : $message="Résultat(s) trouvé(s) :";
     
